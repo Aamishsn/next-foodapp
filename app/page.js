4ddc519156ac49e945645df2b2a81667a08cb0ce
Aamishsn <aamishsn@gmail.com>
2024-08-19 19:23:56 +0500

    main page added and hero section completed

--- app/page.js
+++ app/page.js
@@ -1,9 +1,25 @@
+import classes from "./page.module.css";
+import Link from "next/link";
+import ImageSlideshow from "../components/SlideShow";
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}>
+        <ImageSlideshow/>
+        </div>
+
+        <div>
+          <div className={classes.hero}>
+            <h1>next level food for next level Foodies</h1>
+            <p>Taste and Share food from all around the world</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </div>
+      </header>
+    </>
   );
 }
