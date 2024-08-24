95b50d7bc7e072f10b660bb5e19a7ffd94069c11
Aamishsn <aamishsn@gmail.com>
2024-08-24 19:37:54 +0500

    nav adjustment done

--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -1,22 +1,39 @@
+"use client";
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./MainHeader.module.css";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 const MainHeader = () => {
+  const pathname = usePathname();
   return (
     <header className={classes.header}>
       <Link className={classes.logo} href="/">
-        <Image src={logoImg} alt="Logo" priority/>
+        <Image src={logoImg} alt="Logo" priority />
         Nextlevel Food
       </Link>
 
       <nav className={classes.nav}>
         <ul>
           <li>
-            <Link href="/meals">Browse Meals</Link>
+            <Link
+              href="/meals"
+              className={
+                pathname.startsWith("/meals") ? classes.active : undefined
+              }
+            >
+              Browse Meals
+            </Link>
           </li>
           <li>
-            <Link href="/community"> Foodies Community</Link>
+            <Link
+              href="/community"
+              className={
+                pathname.startsWith("/community") ? classes.active : undefined
+              }
+            >
+              Foodies Community
+            </Link>
           </li>
         </ul>
       </nav>
